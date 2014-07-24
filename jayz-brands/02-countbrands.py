2dddc207fd512e63a76c2ac3f24198ad6790e81f
Kat Chuang <katychuang@gmail.com>
2014-07-24 16:42:47 -0400

    create function for reading lyrics files

--- jayz-brands/02-countbrands.py
+++ jayz-brands/02-countbrands.py
@@ -1,1 +1,20 @@
 # Module for parsing lyrics and counting brand mentions
+
+
+import nltk
+import re
+import codecs
+
+lyricsfile = "lyrics/2013-tom-ford-lyrics.txt"
+
+def openFile(lyricsfile, data=[]):
+    with open(lyricsfile, 'rb') as textfile:
+        stripped = (row.strip() for row in textfile if not row.startswith(('[')))
+        data = [row for row in stripped if not row == ""]
+
+    return data
+
+
+raw = openFile(lyricsfile) 
+
+
