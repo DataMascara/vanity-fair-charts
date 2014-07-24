f7cea991de883c7485dd023ef622966c0c85a9fa
Kat Chuang <katychuang@gmail.com>
2014-07-24 17:02:52 -0400

    write basic functions for tokenizing and writing file

--- jayz-brands/02-countbrands.py
+++ jayz-brands/02-countbrands.py
@@ -10,11 +10,29 @@ lyricsfile = "lyrics/2013-tom-ford-lyrics.txt"
 def openFile(lyricsfile, data=[]):
     with open(lyricsfile, 'rb') as textfile:
         stripped = (row.strip() for row in textfile if not row.startswith(('[')))
-        data = [row for row in stripped if not row == ""]
-
+        data = [row for row in stripped if not len(row) == 0]
     return data
 
+def tokenize(lines):
+	str1 = ''.join(lines)
+	tokens = nltk.word_tokenize(str1) # tokenizes the raw string
+	text = nltk.Text(tokens)          # generate the text object
+	clean_text = ["".join(re.split("([.,;:!?'`-])", word)) for word in text]
+	fdist = nltk.FreqDist(clean_text)
+	return fdist
 
-raw = openFile(lyricsfile) 
+def writeFile(fdist):
+	outputname = "frequencies/2013-tom-ford-lyrics.txt"
+	with codecs.open(outputname, 'w', 'utf-8') as l:
+		[l.write(word+"\n") for word in fdist]
 
+lines = openFile(lyricsfile) 
+fdist = tokenize(lines)
+
+print "Distribution before normalization of stop words\n", fdist, "\n"
+writeFile(fdist)
+
+
+
+		
 
