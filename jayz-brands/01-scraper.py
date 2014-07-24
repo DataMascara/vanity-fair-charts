8839529c3f12ac4fbdae3bb4a746bd256a0ea98d
Kat Chuang <katychuang@gmail.com>
2014-07-24 01:15:58 -0400

    loop through all albums

--- jayz-brands/01-scraper.py
+++ jayz-brands/01-scraper.py
@@ -19,47 +19,70 @@ __email__ = "katychuang@gmail.com"
 __status__ = "Development"
 
 
- 
-# grab page
-root = "http://rapgenius.com"
-path = "/albums/Jay-z/American-gangster"
-page = requests.get(root+path)
-year = "2007"
- 
-# load downloaded page into BS
-html = BeautifulSoup(page.text)
-song_links = []
-# let's grab all the links in .song_list elements
-song_links.extend((a['href'] for a in html.select(".song_list li a")))
- 
-songs = list(set(song_links))
-lyrics = []
-for song in songs:
-    
-    # make a request to grab each page linked to
-    page = requests.get(root+song)
-    outputname = "lyrics/" + year + song[6:] + ".txt"
-    print outputname
+
+root = "http://genius.com"
+
+albums = [
+["1996", "Reasonable Doubt", "/albums/Jay-z/Reasonable-doubt"], 
+["1997", "In My Lifetime", "/albums/Jay-z/In-my-lifetime-vol-1"],
+["1998", "Vol 2... Hard Knock Life", "/albums/Jay-z/Vol-2-hard-knock-life"],
+["1999", "Vol 3... Life and Times of S. Carter", "/albums/Jay-z/Vol-3-life-and-times-of-s-carter"],
+["2000", "The Dynasty: Roc La Familia", "/albums/Jay-z/The-dynasty-roc-la-familia"],
+["2001", "The Blueprint", "/albums/Jay-z/The-blueprint"], 
+["2002", "The Best of Both Worlds", "/albums/Jay-z/The-best-of-both-worlds"],
+["2002", "Blueprint 2", "/albums/Jay-z/The-blueprint-2-the-gift-the-curse"],
+["2003", "The Black Album", "/albums/Jay-z/The-black-album"],
+["2004", "Unfinished Business", "/albums/Jay-z/Unfinished-business"],
+["2006", "Kingdom Come", "/albums/Jay-z/Kingdom-come"],
+["2007", "American Gangster", "/albums/Jay-z/American-gangster"], 
+["2009", "The Blueprint 3", "/albums/Jay-z/The-blueprint-3"],
+["2011", "Watch the Throne", "/albums/Kanye-west/Watch-the-throne"],
+["2013", "Magna Carta... Holy Grail", "/albums/Jay-z/Magna-carta-holy-grail"],
+]
+
+# download lyrics from each album
+def songLyrics(year, page):
+
+    # load downloaded page into BS
     html = BeautifulSoup(page.text)
-    # the div with the content we want has the class "lyrics"
-    lines = html.select(".lyrics")
-    pebble = []
-    
-    for line in lines:
-        # discard all the tags but save their content
-        [a.unwrap() for a in line.select("a")]
-        [br.unwrap() for br in line.select("br")]
-        pebble.append(line)
-        # section it out by [*]
-        # if the [*] has no artist name (indicated by a colon) or the artist name "2 Chainz"
-        # we want those lyrics
-        # otherwise, discard
-    lyrics.extend(pebble) # save it all into lyrics list
-    
-    with codecs.open(outputname, 'w', 'utf-8') as l:
-      [l.write(lyric.get_text()) for lyric in lyrics]
-    
-    lyrics = [] # clear it after each file
+    song_links = []
+
+    # grab all the links in .song_list elements
+    song_links.extend((a['href'] for a in html.select(".song_list li a")))
+     
+    songs = list(set(song_links))
+    lyrics = []
+    for song in songs:
+        
+        # make a request to grab each page linked to
+        page = requests.get(root+song)
+        outputname = "lyrics/" + year + song[6:] + ".txt"
+        print outputname
+
+        html = BeautifulSoup(page.text)
+        # the div with the content we want has the class "lyrics"
+        lines = html.select(".lyrics")
+        pebble = []
+        
+        for line in lines:
+            [a.unwrap() for a in line.select("a")]
+            [br.unwrap() for br in line.select("br")]
+            pebble.append(line)
+
+        lyrics.extend(pebble) # save it all into lyrics list
+        
+        with codecs.open(outputname, 'w', 'utf-8') as l:
+          [l.write(lyric.get_text()) for lyric in lyrics]
+        
+        lyrics = [] # clear it after each file
+
+# go through list of albums and grab page
+for album in albums:
+    year = album[0]
+    path = album[2]
+    page = requests.get(root+path)
+    songLyrics(year, page)
 
 # ------
 # Credits to @jiko's example https://gist.github.com/jiko/7305816
+
