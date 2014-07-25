2340175232eb1ea3d5de75407db9ad6aa4c5859f
Kat Chuang <katychuang@gmail.com>
2014-07-24 21:46:51 -0400

    fix whitespace

--- jayz-brands/02-countbrands.py
+++ jayz-brands/02-countbrands.py
@@ -1,10 +1,23 @@
-# Module for parsing lyrics and counting brand mentions
+#!/usr/bin/env python
+
+""" This module parsing lyrics and counts brand mentions
+"""
 
 
 import nltk
 import re
 import codecs
 import glob
+
+
+__author__ = "Kat Chuang @katychuang"
+__copyright__ = "Copyright 2014"
+__credits__ = ["Kat Chuang"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Kat Chuang"
+__email__ = "katychuang@gmail.com"
+__status__ = "Development"
 
 
 brands = ["Mercedes Benz", "Cristal", "Lexus", "Maybach", "BMW", "Gucci", "Glock", "Bentley", "Nike",
@@ -17,47 +30,46 @@ def openFile(lyricsfile, data=[]):
     return data
 
 def tokenize(lines, normalize=False):
-	str1 = ''.join(lines)
-	tokens = nltk.word_tokenize(str1) # tokenizes the raw string
-	text = nltk.Text(tokens)          # generate the text object
+    str1 = ''.join(lines)
+    tokens = nltk.word_tokenize(str1) # tokenizes the raw string
+    text = nltk.Text(tokens)          # generate the text object
 
-	sw = [",", "(", ")", "'"]
+    sw = [",", "(", ")", "'"]
 
-	if normalize:
-		from nltk.corpus import stopwords
+    if normalize:
+        from nltk.corpus import stopwords
 
-		stopwords = stopwords.words("english")
-		more_stopwords = ["", "ass", ",", "fuck", "nigga", "niggas", "oh", "i", "'s"]
+        stopwords = stopwords.words("english")
+        more_stopwords = ["", "ass", ",", "fuck", "nigga", "niggas", "oh", "i", "'s"]
 
-		sw = more_stopwords + stopwords
-	
-	clean_text = ["".join(re.split("([.,;:!?`-])", word.lower())) for word in text if not word  in sw]
-	
-	fdist = nltk.FreqDist(clean_text)
-	return fdist
+        sw = more_stopwords + stopwords
+
+    clean_text = ["".join(re.split("([.,;:!?`-])", word.lower())) for word in text if not word  in sw]
+
+    fdist = nltk.FreqDist(clean_text)
+    return fdist
 
 def writeFile(fdist, outputname):
-	with codecs.open(outputname, 'w') as l:
-		[l.write(word + "\t\t\t" + str(fdist[word]) + "\n") for word in fdist]
+    with codecs.open(outputname, 'w') as l:
+        [l.write(word + "\t\t\t" + str(fdist[word]) + "\n") for word in fdist]
 
 
 for song in glob.glob('lyrics/*.txt'):
     if "album-art" not in song:
 
-	    lines = openFile(song)
+        lines = openFile(song)
 
-	    print str(len(lines)) + " lines read from " + song
+        print str(len(lines)) + " lines read from " + song
 
-	    fdist = tokenize(lines, False)
-	    print "Distribution before normalization of stop words: ", str(len(fdist))
-	    output = song.replace("lyrics", "frequencies")
-	    writeFile(fdist, output)
+        fdist = tokenize(lines, False)
+        print "Distribution before normalization of stop words: ", str(len(fdist))
+        output = song.replace("lyrics", "frequencies")
+        writeFile(fdist, output)
 
-	    fdistN = tokenize(lines, True)
-	    print "Distribution after normalization of stop words: ", str(len(fdistN)), "\n"
-	    output = song.replace("lyrics", "frequencies-normalized")
-	    writeFile(fdistN, output)
-
+        fdistN = tokenize(lines, True)
+        print "Distribution after normalization of stop words: ", str(len(fdistN)), "\n"
+        output = song.replace("lyrics", "frequencies-normalized")
+        writeFile(fdistN, output)
 
 
 # x = fdist.tabulate(samples=range(10), cumulative=True)
