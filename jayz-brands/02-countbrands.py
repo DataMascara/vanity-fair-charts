13ff49562ab1631368d472cf02a695168b144362
Kat Chuang <katychuang@gmail.com>
2014-08-07 17:31:26 -0400

    remove white space, lowercase brands

--- jayz-brands/02-countbrands.py
+++ jayz-brands/02-countbrands.py
@@ -3,7 +3,6 @@
 """ This module parsing lyrics and counts brand mentions
 """
 
-
 import nltk
 import re
 import codecs
@@ -20,9 +19,11 @@ __email__ = "katychuang@gmail.com"
 __status__ = "Development"
 
 
-brands = ["Mercedes Benz", "Cristal", "Lexus", "Maybach", "BMW", "Gucci", "Glock", "Bentley", "Nike",
-"Range Rover", "Rolex", "Porsche", "Versace", "Tom Ford", "Intratec"]
 # Brands identified by Vanity Fair
+brands = ["mercedes benz", "cristal", "lexus", "maybach", "bmw", "gucci", 
+          "glock", "bentley", "nike", "range rover", "rolex", "porsche", 
+          "versace", "tom ford", "intratec"]
+
 csvOutput=[]
 
 
@@ -30,20 +31,15 @@ def openFile(lyricsfile, data=[]):
     with open(lyricsfile, 'rb') as textfile:
         stripped = (row for row in textfile if not row.startswith(('[')))
         data = [row for row in stripped if not len(row) == 0]
+
     return data
 
-def tally(lines, song=""):
+def tally(lines, song=""):    
     str1 = ''.join(lines)
-
-    tally = {}
-
     for product in brands:
         if str1.count(product) > 1:
-            tally[product]=str1.count(product)
             print "{}\t  {}x\t{}".format(product.strip(), str1.count(product), 
                                 " ".join(song[7:-11].split("-")))
-
-    return tally
 
 def tokenize(lines, normalize=False, song=""):
     str1 = ''.join(lines)
@@ -62,7 +58,7 @@ def tokenize(lines, normalize=False, song=""):
 
         sw = more_stopwords + stopwords
 
-    clean_text = ["".join(re.split("([.,;:!?`-])", word.lower())) for word in text if not word  in sw]
+    clean_text = ["".join(re.split("([.,;:!?`-])", word.lower())) for word in text if not word in sw]
 
     fdist = nltk.FreqDist(clean_text)
     return fdist
@@ -98,6 +94,8 @@ for song in glob.glob('lyrics/*.txt'):
         # print "Distribution after normalization of stop words: ", str(len(fdistN)), "\n"
         output = song.replace("lyrics/", "frequencies-normalized/")
         writeFile(fdistN, output, song[7:11], " ".join(song[12:-4].split("-")))
+
+
 
 with open('frequencies.csv', 'wb') as csvfile:
     w = csv.writer(csvfile, delimiter=',')
