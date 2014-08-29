5c8957b959f916c5bc8434a95123be3b67cfe9dd
katychuang <katychuang@gmail.com>
2014-08-29 19:01:22 -0400

    content via react

---
+++ js/content.js
@@ -0,0 +1,44 @@
+/** @jsx React.DOM */
+
+var MyContent = React.createClass({
+  render: function() {
+    var title = "Vanity Fair Charts";
+    var desc = "Recreate charts published in Vanity Fair with python!";
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+            <p>
+                {desc}
+            </p>
+            <h2>
+               Luxury brands mentioned in Jay-Z's songs 
+            </h2>
+            <p>
+		
+            </p>
+	    <h2>
+	       Popular Baby Names
+	    </h2>
+	    <p>
+	
+	    </p>
+	    <h2>
+		Color of the Year
+	    </h2>
+	    <p>
+
+	    </p>
+        </div>
+    );
+  }
+});
+
+
+React.renderComponent(
+  <MyContent />,
+  document.getElementById('content')
+);
+
+
